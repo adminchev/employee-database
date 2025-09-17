117ee5a91208cff1663412ae6ee47cad7ce4b929
Alexander <adminchev@proton.me>
2025-09-17 08:21:23 +0300

    Project skeleton

---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) {
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n - create new file\n");
+	printf("\t -f - (required) path to file\n");
+}
+
+int main(int argc, char *argv[]) { 
+	char *filepath = NULL;
+	bool newfile = false;
+	int c = 0;
+
+	while (( c = getopt(argc, argv, "nf:")) != -1 ) {
+		switch(c){
+			case 'n':
+				newfile = true;
+				break;
+
+			case 'f':
+				filepath = optarg;
+				break;
+
+			case '?':
+				printf("Unknown option -%c\n.", c);
+				break;
+
+			default:
+				return -1;
+		}
+	}
+
+	if (filepath == NULL) {
+		printf("Filepath is a required argument.\n");
+		print_usage(argv);
+		return 0;
+	}
+
+	printf("Newfile : %d\n", newfile);
+	printf("Filepath: %s\n", filepath);
+}
