5722669ebb71a991e0dd43e20755b3ff7bede90e
Alexander Minchev <adminchev@proton.me>
2025-09-19 18:53:31 +0300

    refactor(db): update add_employee to use double pointer

--- include/parse.h
+++ include/parse.h
@@ -1,7 +1,10 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-#define HEADER_MAGIC 0x41444D44
+#define HEADER_MAGIC 0x4c4c4144
+#define NAME_LEN 256
+#define ADDRESS_LEN 256
+
 
 struct dbheader_t {
 	unsigned int magic;
@@ -11,8 +14,8 @@ struct dbheader_t {
 };
 
 struct employee_t {
-	char name[256];
-	char address[256];
+	char name[NAME_LEN];
+	char address[ADDRESS_LEN];
 	unsigned int hours;
 };
 
@@ -21,6 +24,6 @@ int validate_db_header(int fd, struct dbheader_t **headerOut);
 int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
 int output_file(int fd, struct dbheader_t *, struct employee_t *employees);
 void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring);
+int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring);
 
 #endif
